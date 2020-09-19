562595a9495569b1d9c2654b6b652064c1443cbb
roshan.birar <roshan.birar@nuance.com>
2020-09-19 21:28:48 +0530

    Update build

---
+++ build/precache-manifest.570b36dcb47b28aeef203b197d72ac72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ef34a8aae3c18bf837a3930c6835311",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23fa2cc549e03941f1d9",
+    "url": "/static/css/main.1a388da1.chunk.css"
+  },
+  {
+    "revision": "e9a5cb79d6d8f49cda99",
+    "url": "/static/js/2.179502a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.179502a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23fa2cc549e03941f1d9",
+    "url": "/static/js/main.1b085ffb.chunk.js"
+  },
+  {
+    "revision": "596d4c2aa7a73ea47457",
+    "url": "/static/js/runtime-main.d0525b3b.js"
+  },
+  {
+    "revision": "227bb238002d629f1a07b4ed48c2bf36",
+    "url": "/static/media/ipl2020.227bb238.jfif"
+  }
+]);
